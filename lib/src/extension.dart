ec32655583ee3fa924f280dee1816dde7fe52997
vnniz <hello@lamnhan.dev>
2023-10-02 20:46:00 +0700

    Add `satisfiedWith` extension for int

--- lib/src/extension.dart
+++ lib/src/extension.dart
@@ -14,9 +14,6 @@ extension SatisfiedVersionEx on String {
     /// [defaultValue] is the default result for `Map` when `appVersion` is not in any range.
     bool defaultValue = false,
 
-    /// [preferTrue] is the preferred value when there are multiple results in `Map`.
-    bool preferTrue = false,
-
     /// [defaultCondition] is the default condition if the compared version is provided without condition.
     ///
     /// `'1.0.0'.satisfiedWith('1.0.0') = '1.0.0'.satisfiedWith('==1.0.0') => true`
@@ -44,7 +41,54 @@ extension SatisfiedVersionEx on String {
         compareWith,
         defaultValue: defaultValue,
         defaultCondition: defaultCondition,
-        preferTrue: preferTrue,
+      );
+    }
+
+    return throw UnsupportedError(
+        'This method only support `String`, `List<String>` and `Map<String, bool`');
+  }
+}
+
+extension SatisfiedWithIntEx on int {
+  /// Extension for the below methods with auto recognition:
+  ///   [SatisfiedVersion.string]
+  ///   [SatisfiedVersion.list]
+  ///   [SatisfiedVersion.map]
+  bool satisfiedWith(
+    /// [compareWith] only supports `String`, `List<String>` and `Map<String, bool>`.
+    ///
+    /// '>=100'; ['>=100', '<=200']; {'>=100': true, '<=200': true}
+    dynamic compareWith, {
+    /// [defaultValue] is the default result for `Map` when `appVersion` is not in any range.
+    bool defaultValue = false,
+
+    /// [defaultCondition] is the default condition if the compared version is provided without condition.
+    ///
+    /// `100.satisfiedWith('100') = 100.satisfiedWith('==100') => true`
+    SatisfiedCondition defaultCondition = SatisfiedCondition.equal,
+  }) {
+    if (compareWith is String) {
+      return SatisfiedVersion.number(
+        this,
+        compareWith,
+        defaultCondition: defaultCondition,
+      );
+    }
+
+    if (compareWith is List<String>) {
+      return SatisfiedVersion.listNumber(
+        this,
+        compareWith,
+        defaultCondition: defaultCondition,
+      );
+    }
+
+    if (compareWith is Map<String, bool>) {
+      return SatisfiedVersion.mapNumber(
+        this,
+        compareWith,
+        defaultValue: defaultValue,
+        defaultCondition: defaultCondition,
       );
     }
 
